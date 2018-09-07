e1384f3a48cc3b6ef96787a9be83cdd09dd5fd10
ankitagnihotri1993 <ankit.ankit.agnihotri@gmail.com>
2018-09-07 20:33:25 +0530

    update

---
+++ test.js
@@ -0,0 +1,22 @@
+"use strict";
+const request = require('request-promise');
+const API_BASE_URI = 'https://services.odata.org/TripPinRESTierService/(S(kxrlfpjz0tsjcnqu1vedklv2))/People';
+
+function getPeople()
+{
+
+   console.log('invoked');0
+   const requestOptions = {
+       uri: API_BASE_URI,
+       json: true
+   };
+
+   // return the promise that sends a request to the Petstore API
+request.get(requestOptions).then(function(response)
+ {
+  console.log('People data '+ JSON.stringify(response));
+
+  });
+
+}
+getPeople();
