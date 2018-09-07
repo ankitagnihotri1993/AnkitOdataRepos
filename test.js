d3514cfaf723d1ff131d8b681a4b06452ffcf735
ankitagnihotri1993 <ankit.ankit.agnihotri@gmail.com>
2018-09-07 21:55:09 +0530

    update

--- test.js
+++ test.js
@@ -19,3 +19,12 @@ request.get(requestOptions).then(function(response)
   });
 
 }
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+myEmitter.emit('event');
